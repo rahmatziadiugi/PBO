c5d50b8eeb5c34bd42cdd7af4a44f35819540337
janu <janu@perdana.connectify>
2016-04-02 00:10:26 +0700

    class petugas

---
+++ Tubes PBO Mantap/src/mainclass/Menu.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mainclass;
+
+/**
+ *
+ * @author janu
+ */
+public class Menu {
+    public void login(){
+        
+       
+    }
+    public void addPetugas(){
+        
+    }
+    
+    public void removePetugas(){
+        
+    }
+    
+    public void addPenyedia(){
+        
+    }
+    
+    public void removePenyedia(){
+        
+    }
+    
+    public void addBarang(){
+        
+    }
+    
+    public void removeBarang(){
+        
+    }
+    
+    public void lupaPassword(){
+        
+    }
+    
+    public void ambilBarang(){
+        
+    }
+    
+    public void tambahStock(){
+        
+    }
+    
+    public void ambilStock(){
+        
+    }
+    
+    public void taruhBarang(){
+        
+    }
+    
+    public void dataBarang() {
+        
+    }
+    
+    public void addGudang(){
+        
+    }
+    
+    public void removeGudang(){
+        
+    }
+    
+    public void editGudang(){
+        
+    }
+    
+    
+}
