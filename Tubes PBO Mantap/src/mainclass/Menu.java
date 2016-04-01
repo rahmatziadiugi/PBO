dc307f310c50ddd251f1091a06f5750c3cbd2dcf
Someone-PC <Someone-PC@Someone-PC>
2016-04-02 01:36:54 +0700

    merapikan main

--- Tubes PBO Mantap/src/mainclass/Menu.java
+++ Tubes PBO Mantap/src/mainclass/Menu.java
@@ -1,3 +1,5 @@
+package mainclass;
+
 import java.util.ArrayList;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,16 +13,14 @@ package mainclass;
  * @author janu
  */
 public class Menu {
-
-private ArrayList<petugas> daftarPetugas=new ArrayList<>();
-private ArrayList<penyedia> daftarPenyedia=new ArrayList<>();
-private ArrayList<gudang> daftarGudang=new ArrayList<>();
-private petugas p;
-private penyedia s;
-private barang b;
-private stock t;
-
-
+    
+    private ArrayList<Petugas> DaftarPetugas = new ArrayList<>();
+    private ArrayList<Penyedia> DaftarPenyedia = new ArrayList<>();
+    private ArrayList<Gudang> DaftarGudang = new ArrayList<>();
+    private Petugas p;
+    private Penyedia s;
+    private Gudang g;
+    private Barang b;
         
     public void login(){
         login
@@ -30,60 +30,62 @@ private stock t;
         
     }
 
-    public void addPetugas(petugas p){
-        daftarPetugas.add(p);
+    public void AddPetugas(Petugas p){
+        this.DaftarPetugas.add(p);
     }
     
-    public void removePetugas(petugas p){
-        daftarPetugas.remove(p);
+    public void RemovePetugas(Petugas p){
+        this.DaftarPetugas.remove(p);
     }
     
-    public void addPenyedia(penyedia s){
-        daftarPenyedia.add(s);
+    public void addPenyedia(Penyedia s){
+        this.DaftarPenyedia.add(s);
     }
     
-    public void removePenyedia(penyedia s){
-        daftarPenyedia.add(s);
+    public void RemovePenyedia(Penyedia s){
+        this.DaftarPenyedia.remove(s);
     }
     
-    public void addBarang(barang b){
-        s.createBarang();
+    public void AddBarang(String Jenis, String NamaBarang, int Stock, int Volume){
+        this.s.CreateBarang(Jenis,NamaBarang,Stock,Volume);
     }
     
-    public void removeBarang(){
-        s.removeBarang();
+    public void RemoveBarang(Barang b){
+        this.s.RemoveBarang(b);
     }
     
-    public void tambahStock(){
-        t.addStock();
+    public void TambahStock(Barang b, int n){
+        this.s.AddStockBarang(b, n);
     }
     
-    public void ambilStock(){
-        t.removeStock();
+    public void ambilStock(Barang b, int n){
+        this.s.RemoveStockBarang(b, n);
     }
     
-    public void ambilBarang(){
-        
+    public void ambilBarang(Barang b, int n){
+        this.g.removeBarang(b, n);
+        this.s.AddStockBarang(b, n);
     }
     
-    public void taruhBarang(){
-        
+    public void taruhBarang(Barang b, int n){
+        this.s.RemoveStockBarang(b, n);
+        this.g.AddBarang(b, n);
     }
     
     public void dataBarang() {
-        for (gudang daftarGudang1:this.daftarGudang){
-            daftarGudang1.viewAllBarang;
+        for (Gudang DaftarGudang1 : this.DaftarGudang){
+            DaftarGudang1.ViewAllBarang();
         }
     }
-    public void tambahGudang(gudang g){
-        daftarGudang.addGudang(g);
+    public void tambahGudang(Gudang g){
+        this.DaftarGudang.add(g);
     }
     
-    public void removeGudang(gudang g){
-        daftarGudang.removeGudang(g);
+    public void RemoveGudang(Gudang g){
+        this.DaftarGudang.remove(g);
     }
     
-    public void editGudang(gudang g){
-        daftarGudang.editGudang(g);
+    public void editGudang(Gudang g){
+        
     }
 }
